5469a8de45d71f5ee2f4f2618c577c0245929222
didierganthier <didierganthierperan@gmail.com>
2021-02-15 16:53:23 -0500

    logout method

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:telegramchatapp/Pages/ChattingPage.dart';
+import 'package:telegramchatapp/main.dart';
 import 'package:telegramchatapp/models/user.dart';
 import 'package:telegramchatapp/Pages/AccountSettingsPage.dart';
 import 'package:telegramchatapp/Widgets/ProgressWidget.dart';
@@ -27,7 +29,18 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    return RaisedButton.icon(onPressed: logoutUser, icon: Icon(Icons.close), label: Text("Sign Out"));
+  }
 
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future <Null> logoutUser() async
+  {
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.disconnect();
+    await googleSignIn.signOut();
+    
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> MyApp()), (Route <dynamic> route) => false);
   }
 }
 
